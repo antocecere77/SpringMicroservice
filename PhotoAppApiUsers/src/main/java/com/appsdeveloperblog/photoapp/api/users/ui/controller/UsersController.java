e39fad9039416909989d8c5a2141eb52f07f877e
antocecere77 <antocecere77@gmail.com>
2020-05-25 14:19:11 +0200

    Access users web service endpoint via eureka discovery service

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controller/UsersController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users")
 public class UsersController {
 
-    @GetMapping("/status/ckeck")
+    @GetMapping("/status/check")
     public String status() {
         return "Working";
     }
