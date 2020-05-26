4821fe9dee1d202d8ed33eb66f3d729dfa232af9
antocecere77 <antocecere77@gmail.com>
2020-05-26 10:11:35 +0200

    Adding method to handle http post request

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.photoapp.api.users.ui.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String crateUser() {
+        return "create user method is called";
     }
 
 }
