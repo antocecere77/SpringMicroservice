1ec6c0b56017409566b6813befc7aecf208909c8
antocecere <antocecere77@gmail.com>
2020-06-20 12:19:36 +0200

    Configure project to use hystrix circuit breaker

--- PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class PhotoAppDiscoveryServiceApplication {
+public class  PhotoAppDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
